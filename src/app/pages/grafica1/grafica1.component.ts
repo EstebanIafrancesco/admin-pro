b5f2feaa2798a867758406407a8b8e397908acfe
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-01-27 23:30:48 -0500

    Donut component has been included and it's logic

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [],
 })
 export class Grafica1Component {
-
+  public labels1: string[] = ['Pisos', 'Baños', 'Cocinas'];
+  public data1 = [
+    [350000, 45000, 100000]
+  ];
+  public labels2: string[] = ['Llantas', 'Multiherramienta', 'Mobiliario'];
+  public data2 = [
+    [12, 45, 100]
+  ];
 }
